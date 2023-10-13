7ba2f251ff504e470f7351ef5625a0c3c7f37e46
Matgui4 <mathisguillotin@gmail.com>
2023-10-13 10:57:59 +0200

    1310

---
+++ FirebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyApub7J6Uhxlqts9jQOutTHwnIqPJKZD84",
+  authDomain: "viaformation-679bc.firebaseapp.com",
+  projectId: "viaformation-679bc",
+  storageBucket: "viaformation-679bc.appspot.com",
+  messagingSenderId: "398795804968",
+  appId: "1:398795804968:web:ca30f66b412b71106d401d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
